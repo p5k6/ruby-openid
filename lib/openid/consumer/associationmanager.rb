8cb6a82655f53f7fe88c6378231be06d45cea0c4
Josh Stanfield <josh.stanfield@livingsocial.com>
2013-07-08 16:10:07 -0600

    google auth fix maybe?

--- lib/openid/consumer/associationmanager.rb
+++ lib/openid/consumer/associationmanager.rb
@@ -339,6 +339,18 @@ module OpenID
         return Association.from_expires_in(expires_in, assoc_handle, secret,
                                            assoc_type)
       end
+
+      def get_association_with_skip_for_google
+        # Google no longer wants the assoc_handle parameter when authenticating by OpenID, and passing it in causes
+        # it to always respond that the request is invalid. To get around this, if we're using google, just don't
+        # collect/create an association, so no handle will be used in the request.
+        # See: https://groups.google.com/forum/#!topic/google-federated-login-api/qXZDD7_K7jU
+        return nil if @server_url == "https://www.google.com/accounts/o8/ud"
+
+        get_association_without_skip_for_google
+      end
+
+      alias_method_chain :get_association, :skip_for_google
     end
   end
 end
--- ruby-openid.gemspec
+++ ruby-openid.gemspec
@@ -3,10 +3,10 @@ require File.expand_path('../lib/openid/version', __FILE__)
 
 Gem::Specification.new do |s|
   s.name = 'ruby-openid'
-  s.author = 'JanRain, Inc'
-  s.email = 'openid@janrain.com'
-  s.homepage = 'https://github.com/openid/ruby-openid'
-  s.summary = 'A library for consuming and serving OpenID identities.'
+  s.author = 'Josh Stanfield; original auth: JanRain, Inc'
+  s.email = 'josh.stanfield@livingsocial.com'
+  s.homepage = 'https://github.com/p5k6/ruby-openid'
+  s.summary = 'A library for consuming and serving OpenID identities; LivingSocial port to fix google auth security issue'
   s.version = OpenID::VERSION
   s.licenses = ["Ruby", "Apache Software License 2.0"]
 
